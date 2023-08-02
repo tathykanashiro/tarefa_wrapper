c4234166c77e4c9dc3f1567ccb2535d4f4fda31f
tathykanashiro <tathymarquesani@gmail.com>
2023-08-02 10:15:25 -0300

    Tarefa

---
+++ TarefaWrapper.java
@@ -0,0 +1,10 @@
+public class TarefaWrapper {
+    public static void main(String args[]){
+
+        int number = 25463;
+        System.out.println(number);
+        Integer number2 = Integer.valueOf(number);
+        System.out.println(number2);
+
+    }
+}
